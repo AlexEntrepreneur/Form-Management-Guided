802d264af565a2b1956b4166706528dfef046be6
AlexEntrepreneur <hello@alexentrepreneur.com>
2020-01-29 09:37:14 +0000

    .

--- app/src/components/StudentForm.js
+++ app/src/components/StudentForm.js
@@ -4,12 +4,42 @@ export default function StudentForm() {
   return (
     <div className="StudentForm">
       <h1>Lambda Student Form</h1>
-      {/* Add your form elements here */}
-      {/* first name */}
-      {/* last name */}
-      {/* username */}
-      {/* password */}
-      {/* rememberpassword */}
+      <form>
+        <label htmlFor="student_firstName">First Name</label>
+        <input
+          type="text"
+          id="student_firstName"
+          name="firstName"
+          placeholder="Enter your first name here"
+        />
+        <label htmlFor="student_lastName">Last Name</label>
+        <input
+          type="text"
+          id="student_lastName"
+          name="lastName"
+          placeholder="Enter your last name here"
+        />
+        <label htmlFor="student_username">Username</label>
+        <input
+          type="text"
+          id="student_username"
+          name="username"
+          placeholder="Enter your username here"
+        />
+        <label htmlFor="student_password">Password</label>
+        <input
+          type="password"
+          id="student_password"
+          name="password"
+          placeholder="Enter your password here"
+        />
+        <label htmlFor="student_remember_pass">Remember password?</label>
+        <input
+          type="checkbox"
+          id="student_remember_pass"
+          name="remember_pass"
+        />
+      </form>
     </div>
   );
 }
